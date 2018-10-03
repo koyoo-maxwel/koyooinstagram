f80db5b2a44fb51cf5b82862e89df3b494ec5a8d
koyoo--maxwel <koyoomaxwel@.com>
2018-10-03 11:49:57 +0300

    connecting to postgresql

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/student/Documents/moringa school projects/Moringa Core/Django Module/week2 IP/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
